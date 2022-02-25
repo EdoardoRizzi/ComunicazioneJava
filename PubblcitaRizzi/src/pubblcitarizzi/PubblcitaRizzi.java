6623d8afcf581309d468f6b384b51826fe320992
EdoardoRizzi <62106054+EdoardoRizzi@users.noreply.github.com>
2022-02-25 13:56:05 +0100

    Librerie
    Aggiunte le varie librerie e creato i progetti

---
+++ PubblcitaRizzi/src/pubblcitarizzi/PubblcitaRizzi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pubblcitarizzi;
+
+import telegramAPI.Test;
+
+/**
+ *
+ * @author rizzi_edoardo
+ */
+public class PubblcitaRizzi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Test t = new Test();
+        t.foo();
+    }
+    
+}
